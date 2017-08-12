0f1d884f4a9946ade94a162263ad3fb0b765ab01
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2017-08-12 21:24:11 +0200

    Add animation for opening app

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const loadingImg = document.querySelector(".loading-img img");
 
 // "global" variables
 let tileClass = "";
+let frameLoadedStatus = false;
 
 // Add event listener to individual tiles
 [].forEach.call(tiles, (tile) => {
@@ -28,31 +29,49 @@ appFrame.addEventListener("load", frameLoaded);
 
 // Logic for opening the app
 function openApp(tile){
+  frameLoadedStatus = false;
   // Get info from tile
   const appTitleText = tile.children[1].innerHTML;
   const appSrc = tile.dataset.src;
   tileClass = tile.classList[2] || tile.classList[1];
 
-  // Open loading screen
+  // Add animation div
+  let tileAnimeDiv = document.createElement("div");
+  tileAnimeDiv.classList.add("animate-tile");
+  tileAnimeDiv.style.top = tile.offsetTop + "px";
+  tileAnimeDiv.style.left = tile.offsetLeft + "px";
+  tileAnimeDiv.style.width = tile.offsetWidth + "px";
+  tileAnimeDiv.style.height = tile.offsetHeight + "px";
+  tileAnimeDiv.classList.add(tileClass);
+
+  tile.appendChild(tileAnimeDiv);
+
+  // Set loading screen
   loadingText.innerHTML = appTitleText;
   loadingImg.src = tile.children[0].src;
   loadingScreen.classList.add(tileClass);
-  loadingScreen.style.display = "grid";
 
-  // Animation
-
-  // Show app
-  appStatus.style.display = "grid";
-  appBody.style.display = "block";
-  tilesContainer.style.display = "none";
-
-  appTitle.innerHTML = appTitleText;
+  // Set app
+  // appTitle.innerHTML = appTitleText;
   appFrame.src = appSrc;
+  appStatus.classList.add(tileClass);
 
-  console.log(tile);
+  // Wait for animation
+  loadingAnimationTimer = setTimeout(() => {
+    // Handle displays
+    if(!frameLoadedStatus) loadingScreen.style.display = "grid";   // Show loading screen
+    tileAnimeDiv.style.display = "none";    // Hide animation div
+    appStatus.style.display = "grid";       // show app-status
+    appBody.style.display = "block";        // show app-body
+    tilesContainer.style.display = "none";  // Hide tilesContainer
+
+    // Remove animation div
+    tile.removeChild(tile.childNodes[tile.childNodes.length-1]);
+  }, 600);
 }
 
 function frameLoaded(){
+  frameLoadedStatus = true;
   // Close loading screen
   loadingScreen.style.display = "none";
   if(loadingScreen.classList.contains(tileClass)) loadingScreen.classList.remove(tileClass);
@@ -62,6 +81,8 @@ function closeApp(){
   appStatus.style.display = "none";
   appBody.style.display = "none";
   tilesContainer.style.display = "grid";
+
+  if(appStatus.classList.contains(tileClass)) appStatus.classList.remove(tileClass);
 
   appFrame.src = "";
 }
